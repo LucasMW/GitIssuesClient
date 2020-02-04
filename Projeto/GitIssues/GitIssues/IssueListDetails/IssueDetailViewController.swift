5cca7285f91892679e4f77cea028cda2182fae4e
LucasMW <great-lucas@hotmail.com>
2020-02-04 19:04:26 -0300

    IssueList Details

--- Projeto/GitIssues/GitIssues/IssueListDetails/IssueDetailViewController.swift
+++ Projeto/GitIssues/GitIssues/IssueListDetails/IssueDetailViewController.swift
@@ -9,22 +9,21 @@
 import UIKit
 
 class IssueDetailViewController: UIViewController {
-
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var userAvatarImage: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var createdLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func browserButtonAction(_ sender: Any) {
     }
-    */
+    
+
+
 
 }
