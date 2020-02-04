d3ae0d771924bf94f20554a1a16fcc94530ca00c
LucasMW <great-lucas@hotmail.com>
2020-02-04 19:08:53 -0300

    changed first screen, workaround bug and proto loading screen

--- Projeto/GitIssues/GitIssues/ViewController.swift
+++ Projeto/GitIssues/GitIssues/ViewController.swift
@@ -7,12 +7,38 @@
 //
 
 import UIKit
+import NVActivityIndicatorView
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, NVActivityIndicatorViewable  {
 
+    var loaded = false
     override func viewDidLoad() {
+        print("Loading!")
         super.viewDidLoad()
+        self.startAnimating( message: "Loading", type: .ballBeat, color: .red, padding: 100, displayTimeThreshold: 4, minimumDisplayTime: 2, backgroundColor: .black, textColor: .systemRed, fadeInAnimation: .none)
+        
+        if !loaded {
+            DispatchQueue.init(label: "xxx").async {
+                sleep(3)
+                DispatchQueue.main.async {
+                    self.stopAnimating()
+                    let first = IssueListViewController()
+                    first.modalPresentationStyle = .overFullScreen
+                                   self.present(first, animated: true) {
+                                       self.loaded = true
+                               }
+                }
+
+
+            }
+        }
+        
         // Do any additional setup after loading the view.
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        print("Did Appear \(loaded)")
+       
+        
     }
 
 
