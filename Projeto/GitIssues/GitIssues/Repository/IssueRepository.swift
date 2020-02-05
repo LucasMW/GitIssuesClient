411ae5715ddaa66582c668f1c8d2fa6b4a679876
LucasMW <great-lucas@hotmail.com>
2020-02-05 14:58:18 -0300

    encapsulates data access

--- Projeto/GitIssues/GitIssues/Repository/IssueRepository.swift
+++ Projeto/GitIssues/GitIssues/Repository/IssueRepository.swift
@@ -8,6 +8,26 @@
 
 import Foundation
 
-class AppMemory {
-    var issues : [IssuesRequest] = []
+
+final class IssueRepository {
+    private init() { }
+    static let main = IssueRepository()
+    var issues : Observable<[IssuesRequest]> = Observable([])
+    
+    func getIsssuesFrom(_ repo : String){
+        print("Calling API")
+        let api = API()
+        api.requestIssues(from: repo) { (requests, err) in
+            guard let requests = requests else {
+                print(err?.localizedDescription ?? "Unknown error")
+                return
+            }
+            self.issues.value = requests
+            print("API Responded")
+        }
+    }
+    func getIssueOf(_ id : Int) ->  IssuesRequest? {
+        let candidates = issues.value.filter({$0.id == id})
+        return candidates.first
+    }
 }
