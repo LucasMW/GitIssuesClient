b9c1332ba1f67a98e1e49a5bbcbf26c3cb3c2c65
LucasMW <great-lucas@hotmail.com>
2020-02-05 14:56:36 -0300

    at least functional

--- Projeto/GitIssues/GitIssues/IssueListDetails/IssueDetailViewController.swift
+++ Projeto/GitIssues/GitIssues/IssueListDetails/IssueDetailViewController.swift
@@ -7,17 +7,59 @@
 //
 
 import UIKit
+import NVActivityIndicatorView
 
-class IssueDetailViewController: UIViewController {
+struct IssueDetailData : Equatable {
+    var title : String
+    var userAvatarImage: UIImage
+    var userName: String
+    var created : String
+    var issueDescription: String
+    
+    static func dummy() -> IssueDetailData {
+        return IssueDetailData(title: "Error: Break in UIApplicationEndBackgroundTaskError() to debug. ", userAvatarImage: UIImage(named: "error-image") ?? UIImage(), userName: "User00001", created: "9/9/9999 12:50:49", issueDescription: """
+        GitIssues[40111:1068823] Can't end BackgroundTask: no background task exists with identifier 2 (0x2), or it may have already been ended. Break in UIApplicationEndBackgroundTaskError() to debug.
+        """)
+    }
+}
+
+class IssueDetailViewController: UIViewController, NVActivityIndicatorViewable {
+    
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var userAvatarImage: UIImageView!
     @IBOutlet weak var userNameLabel: UILabel!
     @IBOutlet weak var createdLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
     
+    var details = IssueDetailData.dummy()
+    var issueId : Int!
+    private var viewModel = IssueDetailsViewModel()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        
+    }
+    func updateValue() {
+        self.titleLabel.text = details.title
+        self.userAvatarImage.image = details.userAvatarImage
+        self.userNameLabel.text = details.userName
+        self.createdLabel.text = details.created
+        self.descriptionLabel.text = details.issueDescription
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.startAnimating()
+        viewModel.issue.observe { (issue) in
+            print("Issue Detail, detail changed")
+            self.details = issue
+            DispatchQueue.main.async {
+                self.updateValue()
+                self.stopAnimating()
+            }
+           
+        }
+        viewModel.loadDetails(id: self.issueId)
+        
         // Do any additional setup after loading the view.
     }
     @IBAction func browserButtonAction(_ sender: Any) {
--- Projeto/GitIssues/GitIssues/IssueListDetails/IssueDetailsViewModel.swift
+++ Projeto/GitIssues/GitIssues/IssueListDetails/IssueDetailsViewModel.swift
@@ -7,3 +7,27 @@
 //
 
 import Foundation
+
+class IssueDetailsViewModel{
+    private var issues: Observable<[IssueDetailData]> = Observable([])
+    var repository = IssueRepository.main
+    var issue: Observable<IssueDetailData> = Observable(IssueDetailData.dummy())
+    
+    init() {
+        repository.issues.observe { (requests) in
+            print("mapping request to details")
+            DispatchQueue.init(label: "zz").async {
+                self.issues.value = requests.mapToIssueDetailData()
+            }
+            print("finish mapping")
+        }
+    }
+    func loadDetails(id : Int){
+        print("Load Details for id \(id)")
+        guard let issue = repository.getIssueOf(id) else {
+            return
+        }
+        print("Loaded")
+        self.issue.value = issue.toIssueDetailData()
+    }
+}
