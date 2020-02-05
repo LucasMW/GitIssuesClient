d65eb9572ae028b8a9a07409852f5b6f682796c9
LucasMW <great-lucas@hotmail.com>
2020-02-05 09:20:19 -0300

    convert api to display structures

---
+++ Projeto/GitIssues/GitIssues/Mapper/IssueMapper.swift
@@ -0,0 +1,34 @@
+//
+//  IssueMapper.swift
+//  GitIssues
+//
+//  Created by Lucas Menezes on 2/5/20.
+//  Copyright © 2020 Lucas Menezes. All rights reserved.
+//
+
+import UIKit
+
+extension IssuesRequest {
+    func toIssueListData() -> IssueListData{
+        let open = self.state == "open"
+        let data = IssueListData(title: self.title ?? "???", open: open)
+        return data
+    }
+    
+    func toIssueDetailData() -> IssueDetailData {
+        let avatar_url = URL(string: self.user?.avatar_url ?? "")
+        let imageData = try? Data(contentsOf: avatar_url!)
+        let image = UIImage(data: imageData!)
+        //to consider: make view model is responsability to tell the view to load imagess from url
+        let data = IssueDetailData(title:  self.title ?? "???", userAvatarImage: image!, userName: self.author_association ?? "???", created: self.created_at ?? "???", issueDescription: self.body ?? "???")
+        return data
+    }
+}
+extension Array where Element == IssuesRequest {
+    func mapToIssueListData() -> [IssueListData]{
+        return self.map({$0.toIssueListData()})
+    }
+    func mapToIssueDetailData() -> [IssueDetailData]{
+        return self.map({$0.toIssueDetailData()})
+    }
+}
